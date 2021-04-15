3540dfbb963091efb381e41c0cda04b890106b32
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-15 15:28:29 -0600

    Change fixture files
    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>

---
+++ spec/requests/recipe_view_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipe Viewpage' do
+  describe 'happy paths' do
+    it 'returns recipe infomation' do
+
+      # get '/recipes?ingredient1=chicken&ingredient2=rice'
+
+      json_response = File.read("spec/fixtures/recipe_call.json")
+      stub_request(:get, "https://pure-chamber-22336.herokuapp.com/recipes?ingredient1=chicken&ingredient2=rice").
+        to_return(status: 200, body: json_response)
+
+        parsed = JSON.parse(json_response, symbolize_names:true)
+
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:data]).to be_a(Hash)
+
+        expect(parsed[:data][:id]).to be_a(String)
+        expect(parsed[:data][:type]).to be_a(String)
+        expect(parsed[:data][:attributes]).to be_a(Hash)
+        expect(parsed[:data][:attributes][:title]).to be_a(String)
+        expect(parsed[:data][:attributes][:cuisine]).to be_a(String)
+        expect(parsed[:data][:attributes][:calories]).to be_a(Integer)
+
+    end
+  end
+end
